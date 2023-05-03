eeb4fd550ea49c733c164d23e64c04a94335b2c2
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-05-03 17:18:10 +0200

    links

--- src/scenes/Navbar/Link.tsx
+++ src/scenes/Navbar/Link.tsx
@@ -3,11 +3,18 @@ import AnchorLink from "react-anchor-link-smooth-scroll"
 
 type Props = {
     page: string;
+    selectedPage: string;
+    setSelectedPage: (value: string) => void;
 }
 
-const Link = ({page}: Props) => {
+const Link = ({page, selectedPage, setSelectedPage}: Props) => {
+    const lowerCasePage = page.toLowerCase().replace(/ /g, "")
   return (
-    <AnchorLink>
+    <AnchorLink
+    className={`${selectedPage === lowerCasePage ? "text-primary-500" : ""} transition-duration-500 hover:text-primary-500`}
+    href={`#${lowerCasePage}`}
+    onClick={() => setSelectedPage(lowerCasePage)}
+    >
         {page}
     </AnchorLink>
   )
