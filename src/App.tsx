cf355d5e7c08e65b7db78d245c0e7d2669736e95
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-05-04 19:20:42 +0200

    changes on navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import Navbar from '@/scenes/Navbar';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { SelectedPage } from '@/shared/types';
 
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const hangleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if(window.scrollY !== 0) {
+        setIsTopOfPage(false);
+      }
+    }
+    window.addEventListener('scroll', hangleScroll);
+    return () => window.removeEventListener('scroll', hangleScroll);
+  }, []);
 
   return (
     <div className="app bg-gray-20">
-      <Navbar 
+      <Navbar
+      isTopOfPage={isTopOfPage} 
       selectedPage={selectedPage}
       setSelectedPage={setSelectedPage}
       />
