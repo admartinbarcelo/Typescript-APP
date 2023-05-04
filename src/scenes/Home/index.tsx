1ae3aa750cc2350e8bb0bc6f47af18786d98b482
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-05-04 20:32:01 +0200

    home progress

--- src/scenes/Home/index.tsx
+++ src/scenes/Home/index.tsx
@@ -6,6 +6,7 @@ import SponsorRedBull from "@/assets/SponsorRedBull.png";
 import SponsorForbes from "@/assets/SponsorForbes.png";
 import SponsorFortune from "@/assets/SponsorFortune.png";
 import { SelectedPage } from "@/shared/types";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
@@ -14,15 +15,15 @@ type Props = {
 const Home = ({ setSelectedPage }: Props) => {
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
   return (
-    <section id="home" className="br-gray-20 gap-16 py-10 md:h-full md:pb-0">
+    <section id="home" className="bg-gray-20 gap-16 py-10 md:h-full md:pb-0">
       {/* IMAGE AND MAIN HEADER */}
-      <div>
+      <div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6 ">
         {/* MAIN HEADER */}
-        <div>
+        <div className="z-10 mt-32 md:basis-3/5">
           {/* HEADINGS */}
-          <div>
-            <div>
-              <div>
+          <div className="md:-mt-20">
+            <div className="relative">
+              <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1]  md:before:content-evolvetext">
                 <img src={HomePageText} alt="home__page__text" />
               </div>
             </div>
@@ -35,11 +36,33 @@ const Home = ({ setSelectedPage }: Props) => {
           {/* ACTIONS */}
           <div>
             <ActionButton setSelectedPage={setSelectedPage}>Join Now</ActionButton>
+            <AnchorLink 
+            className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+            onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+            href={`#${SelectedPage.ContactUs}`}
+            >
+              <p>Learn More</p>
+            </AnchorLink>
           </div>
         </div>
         {/* IMAGE */}
-        <div></div>
+        <div>
+          <img src={HomePageGraphic} alt="home__page__graphic" />
+        </div>
       </div>
+
+      {/* SPONSORS */}
+      {isAboveMediumScreens && (
+        <div className="h-[150px] w-full bg-primary-100 py-10">
+          <div className="mx-auto w-5/6">
+            <div className="flex w-3/5 items-center justify-between gap-8">
+              <img alt="redbull-sponsor" src={SponsorRedBull} />
+              <img alt="forbes-sponsor" src={SponsorForbes} />
+              <img alt="fortune-sponsor" src={SponsorFortune} />
+            </div>
+          </div>
+        </div>
+      )}
     </section>
   );
 };
