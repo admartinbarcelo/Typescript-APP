b50b787dcf32432d5ad9de63dccb35db7f62d6a2
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-05-05 13:37:32 +0200

    incomming benefits

--- src/scenes/Benefits/index.tsx
+++ src/scenes/Benefits/index.tsx
@@ -7,57 +7,131 @@ import {
 } from "@heroicons/react/24/solid";
 import HText from "@/shared/HText";
 import Benefit from "./Benefit";
+import ActionButton from "@/shared/ActionButton";
+import BenefitsPageGraphic from "@/assets/BenefitsPageGraphic.png";
 
-
-const benefits: Array <BenefitType> = [
-    {
-        icon: <HomeModernIcon className="w-6 h6"/>,
-        title: "Modern Equipment",
-        description: "We provide modern equipment to help you achieve your fitness goals."
-    },
-    {
-        icon: <UserGroupIcon className="w-6 h6"/>,
-        title: "Modern Equipment",
-        description: "We provide modern equipment to help you achieve your fitness goals."
-    },
-    {
-        icon: <AcademicCapIcon className="w-6 h6"/>,
-        title: "Modern Equipment",
-        description: "We provide modern equipment to help you achieve your fitness goals."
-    }
-]
+const benefits: Array<BenefitType> = [
+  {
+    icon: <HomeModernIcon className="h6 w-6" />,
+    title: "Modern Equipment",
+    description:
+      "We provide modern equipment to help you achieve your fitness goals.",
+  },
+  {
+    icon: <UserGroupIcon className="h6 w-6" />,
+    title: "Diverse Classes",
+    description:
+      "We provide diverse classes to help you achieve your fitness goals.",
+  },
+  {
+    icon: <AcademicCapIcon className="h6 w-6" />,
+    title: "Expert Trainers",
+    description:
+      "We provide expert trainers to help you achieve your fitness goals.",
+  },
+];
+const container = {
+  hidden: {},
+  visible: {
+    transition: { staggerChildren: 0.2 },
+  },
+};
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
 
-const Benefits = ({setSelectedPage}: Props) => {
+const Benefits = ({ setSelectedPage }: Props) => {
   return (
     <section id="benefits" className="mx-auto min-h-full w-5/6 py-20">
       <motion.div
         onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
       >
         {/* HEADER */}
-        <div className="md:my-5 md:my:w-3/5">
-            <HText>MORE THAN JUST A GYM</HText>
-            <p className="my-5 text-sm">
+        <motion.div
+          className="md:my:w-3/5 md:my-5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -100 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <HText>MORE THAN JUST A GYM</HText>
+          <p className="my-5 text-sm">
             We provide world class fitness equipment, trainers and classes to
             get you to your ultimate fitness goals with ease. We provide true
             care into each and every member.
-            </p>
-        </div>
+          </p>
+        </motion.div>
         {/* BENEFITS */}
-        <div className="md:flex items-center justify-between gap-8 mt-5">
-            {benefits.map((benefit: BenefitType) => (
-                <Benefit
-                key={benefit.title}
-                icon={benefit.icon}
-                title={benefit.title}
-                description={benefit.description}
-                setSelectedPage={setSelectedPage}
-                />
-            ))}
-        </div>
+        <motion.div
+          className="mt-5 items-center justify-between gap-8 md:flex"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={container}
+        >
+          {benefits.map((benefit: BenefitType) => (
+            <Benefit
+              key={benefit.title}
+              icon={benefit.icon}
+              title={benefit.title}
+              description={benefit.description}
+              setSelectedPage={setSelectedPage}
+            />
+          ))}
+        </motion.div>
+        {/* DESCRIPTION & GRAPHICS */}
+        <div>
+          {/* GRAPHIC */}
+          <img
+          className="mx-auto"
+          alt="benefits__page__graphic"
+          src={BenefitsPageGraphic}
+          />
+          {/* DESCRIPCION */}
+          <div>
+            {/* TITLE */}
+            <div className="relative">
+              <div className="before:absolute before:-left-20 before:-top-20 before:z-[1] before:content-abstractwaves">
+                <div>
+                  <HText>
+                    MILLIONS OF HAPPY MEMBERS GETTING{" "}
+                    <span className="text-primary-500">FIT</span>
+                  </HText>
+                </div>
+              </div>
+            </div>
+            {/* DESCRIPTION */}
+            <div>
+              <p className="my-5">
+                Nascetur aenean massa auctor tincidunt. Iaculis potenti amet
+                egestas ultrices consectetur adipiscing ultricies enim. Pulvinar
+                fames vitae vitae quis. Quis amet vulputate tincidunt at in
+                nulla nec. Consequat sed facilisis dui sit egestas ultrices
+                tellus. Ullamcorper arcu id pretium sapien proin integer nisl.
+                Felis orci diam odio.
+              </p>
+              <p className="mb-5">
+                Fringilla a sed at suspendisse ut enim volutpat. Rhoncus vel est
+                tellus quam porttitor. Mauris velit euismod elementum arcu neque
+                facilisi. Amet semper tortor facilisis metus nibh. Rhoncus sit
+                enim mattis odio in risus nunc.
+              </p>
+            </div>
+            {/* BUTTON */}
+            <div className="relative mt-16">
+            <div className="before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles">
+                <ActionButton setSelectedPage={setSelectedPage}>
+                  Join Now
+                </ActionButton>
+              </div>
 
+            </div>
+          </div>
+        </div>
       </motion.div>
     </section>
   );
