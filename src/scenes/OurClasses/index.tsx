3874d7993c6521c57e53bf649e2ef208fd2f3382
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-05-08 20:48:42 +0200

    added some styles with tailwind

--- src/scenes/OurClasses/index.tsx
+++ src/scenes/OurClasses/index.tsx
@@ -18,6 +18,20 @@ const OurClasses = ( {setSelectedPage} : Props) => {
     <motion.div
       onViewportEnter={() => setSelectedPage(SelectedPage.OurClasses)}
     >
+      <motion.div
+      className="mx-auto w-5/6"
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true, amount: 0.5 }}
+      transition={{ duration: 0.5 }}
+      variants={{
+        hidden: {opacity: 0, x: -50},
+        visible: {opacity: 1, x: 0}
+      }}
+
+      >
+
+      </motion.div>
 
     </motion.div>
 
