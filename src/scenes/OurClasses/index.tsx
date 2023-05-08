97113f6d0118ade21471229c911402640e36f0a7
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-05-08 20:43:56 +0200

    classes component starts

--- src/scenes/OurClasses/index.tsx
+++ src/scenes/OurClasses/index.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import image1 from '@assets/image1.png'
+import image2 from '@assets/image2.png'
+import image3 from '@assets/image3.png'
+import image4 from '@assets/image4.png'
+import image5 from '@assets/image5.png'
+import image6 from '@assets/image6.png'
+import { motion } from "framer-motion";
+import { SelectedPage } from "@/shared/types";
 
-type Props = {};
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
 
-const OurClasses = (props: Props) => {
-  return <div>OurClasses</div>;
+};
+
+const OurClasses = ( {setSelectedPage} : Props) => {
+  return <section id="ourclasses" className="w-full bg-primary-100 py-40">
+    <motion.div
+      onViewportEnter={() => setSelectedPage(SelectedPage.OurClasses)}
+    >
+
+    </motion.div>
+
+  </section>
 };
 
 export default OurClasses;
