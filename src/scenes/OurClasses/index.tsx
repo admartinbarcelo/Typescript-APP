30ebfd4f505b859fab372ad73bbfd5b786d04a6d
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-05-09 12:01:51 +0200

    adding photos to classes

--- src/scenes/OurClasses/index.tsx
+++ src/scenes/OurClasses/index.tsx
@@ -1,11 +1,46 @@
-import image1 from '@assets/image1.png'
-import image2 from '@assets/image2.png'
-import image3 from '@assets/image3.png'
-import image4 from '@assets/image4.png'
-import image5 from '@assets/image5.png'
-import image6 from '@assets/image6.png'
+import image1 from '@/assets/image1.png'
+import image2 from '@/assets/image2.png'
+import image3 from '@/assets/image3.png'
+import image4 from '@/assets/image4.png'
+import image5 from '@/assets/image5.png'
+import image6 from '@/assets/image6.png'
 import { motion } from "framer-motion";
-import { SelectedPage } from "@/shared/types";
+import { SelectedPage, ClassType } from "@/shared/types";
+import HText from '@/shared/HText'
+import Class from './Class'
+
+const classes: Array <ClassType> = [
+  {
+    name: "Weightlifting Classes",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ",
+    image: image1,
+  },
+  {
+    name: "Yoga Classes",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ",
+    image: image2,
+  },
+  {
+    name: "Ab Core Classes",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ",
+    image: image3,
+  },
+  {
+    name: "Adventure Classes",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ",
+    image: image4,
+  },
+  {
+    name: "Fitness Classes",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ",
+    image: image5,
+  },
+  {
+    name: "Training Classes",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ",
+    image: image6,
+  },
+]
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
@@ -29,8 +64,28 @@ const OurClasses = ( {setSelectedPage} : Props) => {
       }}
 
       >
+        <div>
+          <HText>Our Classes</HText>
+          <p className='py-5'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed  
+          </p>
+        </div>
 
       </motion.div>
+      <div className='mt-10 h-[353px] w-full overflow-x-auto overflow-y-hidden'>
+        <ul className='w-[2800px] whitespace-nowrap'>
+          {classes.map((item: ClassType, index) => (
+            <Class
+              key={`${item.name}-${index}`}
+              name={item.name}
+              description={item.description}
+              image={item.image}
+            />
+          ))}
+
+        </ul>
+
+      </div>
 
     </motion.div>
 
