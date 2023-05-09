b0b88beccc6f07b638f19032ef3caad1e4d26f0d
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-05-09 12:13:44 +0200

    classes only need put text

--- src/scenes/OurClasses/index.tsx
+++ src/scenes/OurClasses/index.tsx
@@ -1,95 +1,96 @@
-import image1 from '@/assets/image1.png'
-import image2 from '@/assets/image2.png'
-import image3 from '@/assets/image3.png'
-import image4 from '@/assets/image4.png'
-import image5 from '@/assets/image5.png'
-import image6 from '@/assets/image6.png'
+import image1 from "@/assets/image1.png";
+import image2 from "@/assets/image2.png";
+import image3 from "@/assets/image3.png";
+import image4 from "@/assets/image4.png";
+import image5 from "@/assets/image5.png";
+import image6 from "@/assets/image6.png";
 import { motion } from "framer-motion";
 import { SelectedPage, ClassType } from "@/shared/types";
-import HText from '@/shared/HText'
-import Class from './Class'
+import HText from "@/shared/HText";
+import Class from "./Class";
 
-const classes: Array <ClassType> = [
+const classes: Array<ClassType> = [
   {
     name: "Weightlifting Classes",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ",
     image: image1,
   },
   {
     name: "Yoga Classes",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ",
     image: image2,
   },
   {
     name: "Ab Core Classes",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ",
     image: image3,
   },
   {
     name: "Adventure Classes",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ",
     image: image4,
   },
   {
     name: "Fitness Classes",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ",
     image: image5,
   },
   {
     name: "Training Classes",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ",
     image: image6,
   },
-]
+];
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
-
 };
 
-const OurClasses = ( {setSelectedPage} : Props) => {
-  return <section id="ourclasses" className="w-full bg-primary-100 py-40">
-    <motion.div
-      onViewportEnter={() => setSelectedPage(SelectedPage.OurClasses)}
-    >
+const OurClasses = ({ setSelectedPage }: Props) => {
+  return (
+    <section id="ourclasses" className="w-full bg-primary-100 py-40">
       <motion.div
-      className="mx-auto w-5/6"
-      initial="hidden"
-      whileInView="visible"
-      viewport={{ once: true, amount: 0.5 }}
-      transition={{ duration: 0.5 }}
-      variants={{
-        hidden: {opacity: 0, x: -50},
-        visible: {opacity: 1, x: 0}
-      }}
-
+        onViewportEnter={() => setSelectedPage(SelectedPage.OurClasses)}
       >
-        <div>
-          <HText>Our Classes</HText>
-          <p className='py-5'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed  
-          </p>
+        <motion.div
+          className="mx-auto w-5/6"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <div className="md:w-3/5">
+            <HText>Our Classes</HText>
+            <p className="py-5">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            </p>
+          </div>
+        </motion.div>
+        <div className="mt-10 h-[353px] w-full overflow-x-auto overflow-y-hidden">
+          <ul className="w-[2800px] whitespace-nowrap">
+            {classes.map((item: ClassType, index) => (
+              <Class
+                key={`${item.name}-${index}`}
+                name={item.name}
+                description={item.description}
+                image={item.image}
+              />
+            ))}
+          </ul>
         </div>
-
       </motion.div>
-      <div className='mt-10 h-[353px] w-full overflow-x-auto overflow-y-hidden'>
-        <ul className='w-[2800px] whitespace-nowrap'>
-          {classes.map((item: ClassType, index) => (
-            <Class
-              key={`${item.name}-${index}`}
-              name={item.name}
-              description={item.description}
-              image={item.image}
-            />
-          ))}
-
-        </ul>
-
-      </div>
-
-    </motion.div>
-
-  </section>
+    </section>
+  );
 };
 
 export default OurClasses;
