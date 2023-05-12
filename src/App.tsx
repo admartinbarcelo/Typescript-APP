30a73e94452ab187c6aced218d071b49c2c770de
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-05-12 12:44:24 +0200

    contact secion

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import Navbar from '@/scenes/Navbar';
 import Home from "@/scenes/Home";
 import Benefits from "@/scenes/Benefits";
-import OurClasses from './scenes/OurClasses';
+import OurClasses from '@/scenes/OurClasses';
+import ContactUs from '@/scenes/ContactUs';
 import { useEffect, useState } from 'react';
 import { SelectedPage } from '@/shared/types';
 
@@ -34,6 +35,7 @@ function App() {
       <Home setSelectedPage={setSelectedPage}/>
       <Benefits setSelectedPage={setSelectedPage}/>
       <OurClasses setSelectedPage={setSelectedPage}/>
+      <ContactUs setSelectedPage={setSelectedPage}/>
     </div>
   )
 }
