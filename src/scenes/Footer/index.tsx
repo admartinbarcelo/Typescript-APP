01db3a1146f85b65e92a56048031527224e921cc
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-05-15 17:23:26 +0200

    actus

--- src/scenes/Footer/index.tsx
+++ src/scenes/Footer/index.tsx
@@ -15,7 +15,7 @@ const Footer = () => {
               <h4 className="font-bold">Links</h4>
               <p className="my-5">About Us</p>
               <p className="my-5">Trainers</p>
-              <p>Policies and Terms</p>
+              <p className="my-5">Policies and Terms</p>
             </div>
             <div className="mt-16 basis-1/4 md:mt-0">
               <h4 className="font-bold">Contact Us</h4>
